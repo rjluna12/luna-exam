d3189e9ace150cbcf55f8e59e7c50358eb2c9250
nikko luna <nikko luna>
2017-03-20 00:27:21 +0800

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 //set port
 app.set('port', (process.env.PORT || 5000));
 //use static files
-app.use(express.static(path.join(__dirname, 'public')));
+
 //express routes
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/app.html'));
